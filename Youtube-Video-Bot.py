d078834f232f4b8878c61726c5b72a20cd27e4d6
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-06-25 18:47:58 +0200

    Add files via upload

--- Youtube-Video-Bot.py
+++ Youtube-Video-Bot.py
@@ -5,28 +5,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.chrome.options import Options
+from random import randint
+from time import sleep
+import os
 import time
 import random
-views = int(input("enter no. of view: "))
-cycle_time = float(input('Enter time for each view:'))
 #view_time = float(input('enter total time for all views: '))
 proxy = random.choice(open('proxies.txt').readlines())
 useragent = random.choice(open('useragents.txt').readlines())
+url = random.choice(open('links.txt').readlines())
 
-for i in range(views):
-    opts1 = Options()
-    opts1.add_argument('--user-agent=%s'% useragent)
+opts1 = Options()
+opts1.add_argument('--user-agent=%s'% useragent)
 #opts1.add_argument('--mute-audio')
 opts1.add_argument('--incognito')
 #opts1.add_argument('--headless')
 opts1.add_argument('--proxy-server=%s'% proxy)
 opts1.add_argument('--start-maximized')
 browser1 = webdriver.Chrome(options=opts1)
-with open('links.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for url in lines:
-    browser1.get(url)
-    time.sleep(cycle_time)
+browser1.get(url)
+time.sleep(randint(36,360))
+os.startfile("launch.exe")
 browser1.quit()
 #time.sleep(10)
 #play_music_1 = browser1.find_element_by_xpath(""" //*[@id="movie_player"]/div[5]/button """)
