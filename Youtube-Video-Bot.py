e3862cde036a9597b59288f22b5d882e72b5fc6a
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-06-28 02:56:36 +0200

    Add files via upload

--- Youtube-Video-Bot.py
+++ Youtube-Video-Bot.py
@@ -7,10 +7,10 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.chrome.options import Options
 from random import randint
 from time import sleep
-import os
 import time
+import os
 import random
-#view_time = float(input('enter total time for all views: '))
+
 proxy = random.choice(open('proxies.txt').readlines())
 useragent = random.choice(open('useragents.txt').readlines())
 url = random.choice(open('links.txt').readlines())
@@ -19,37 +19,18 @@ opts1 = Options()
 opts1.add_argument('--user-agent=%s'% useragent)
 #opts1.add_argument('--mute-audio')
 opts1.add_argument('--incognito')
-#opts1.add_argument('--headless')
 opts1.add_argument('--proxy-server=%s'% proxy)
+#opts1.add_argument('--headless')
 opts1.add_argument('--start-maximized')
+opts1.add_argument('--disable-gpu')
+
 browser1 = webdriver.Chrome(options=opts1)
-browser1.get(url)
+browser1.execute_script("window.location.replace(arguments[0])", url)
 time.sleep(8)
-if len(browser1.find_elements_by_xpath("//button[@class='ytp-large-play-button ytp-button']")) > 0:
-    browser1.find_element_by_xpath("//button[@class='ytp-large-play-button ytp-button']").click()
-    time.sleep(9)
-    if len(browser1.find_elements_by_xpath("//button[@class='ytp-ad-skip-button ytp-button']")) > 0:
-        browser1.find_element_by_xpath("//button[@class='ytp-ad-skip-button ytp-button']").click()
-        time.sleep(randint(45,75))
-        os.startfile("launch.exe")
-        browser1.quit()
-    else:
-        time.sleep(randint(45,75))
-        os.startfile("launch.exe")
-        browser1.quit()
-else:
-    time.sleep(9)
-    if len(browser1.find_elements_by_xpath("//button[@class='ytp-ad-skip-button ytp-button']")) > 0:
-        browser1.find_element_by_xpath("//button[@class='ytp-ad-skip-button ytp-button").click()
-        time.sleep(randint(45,75))
-        os.startfile("launch.exe")
-        browser1.quit()
-    else:
-        time.sleep(randint(45,75))
-        os.startfile("launch.exe")
-        browser1.quit()
-#time.sleep(10)
-#play_music_1 = browser1.find_element_by_xpath(""" //*[@id="movie_player"]/div[5]/button """)
-#play_music_1.click()
-#//*[@id='ad-text:b']
+browser1.find_element_by_xpath(""" //*[@id="content"]/a """).click()
+time.sleep(randint(180,3200))
+os.startfile("launch.exe")
+browser1.quit()
+
+
 # script by thisisawesome1994
--- launch.py
+++ launch.py
@@ -1,2 +1,3 @@
 import os
+import time
 os.startfile("Youtube-Video-Bot.exe")
